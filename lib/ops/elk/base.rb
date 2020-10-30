8ebf80354af4b6ab0376ad32f1571a6737421c7b
Kevin197011 <kevin197011@outlook.com>
2020-10-30 18:46:32 +0800

    update

---
+++ lib/ops/elk/base.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ops
+  module Elk
+    class << self
+      attr_accessor :base_uri
+    end
+  end
+end
---
+++ lib/ops/elk/index.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Ops
+  module Elk
+    class << self
+      def index!(*args); end
+    end
+  end
+end
